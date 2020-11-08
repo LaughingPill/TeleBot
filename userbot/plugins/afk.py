7e15884a00b6169a5234adf4bdbf326edfc4bd7f
LaughingPill <59218577+LaughingPill@users.noreply.github.com>
2020-11-08 14:19:04 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -153,7 +153,7 @@ async def on_afk(event):
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
         message_to_reply = (
-            f"**[AFK]** `Hi, I'm not available.`\n\n**Reason - ** __{reason}__\n\n**Last seen** - __{afk_since}__"
+            f"**[AFK]** `Hello, My Master is currently not available.`\n\n**Reason - ** __{reason}__\n\n**Last seen** - __{afk_since}__"
             if reason
             else f"**[AFK]** `I am unavailable. Please leave your message, I will look into it soon!`\n\n**Last seen** - __{afk_since}__ "
         )
